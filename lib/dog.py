b8d5b2f74c17be7572b8b661b5299575688dad4a
geoffrey kinuthia <geoffreykinuthia10@gmail.com>
2023-06-08 15:03:25 +0300

    Done

--- lib/dog.py
+++ lib/dog.py
@@ -4,5 +4,83 @@ CONN = sqlite3.connect('lib/dogs.db')
 CURSOR = CONN.cursor()
 
 class Dog:
+    def __init__(self, name, breed):
+        self.id = None
+        self.name = name
+        self.breed = breed
     
-    pass
+    @classmethod
+    def create_table(cls):
+        sql = """
+        CREATE TABLE IF NOT EXISTS dogs (
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            breed TEXT
+        )
+        """
+        CURSOR.execute(sql)
+
+    @classmethod
+    def drop_table(cls):
+        sql = "DROP TABLE IF EXISTS dogs"
+        CURSOR.execute(sql)
+
+    def save(self):
+        if self.id is None:
+            CURSOR.execute("INSERT INTO dogs (name, breed) VALUES (?, ?)", (self.name, self.breed))
+            self.id = CURSOR.lastrowid
+        else:
+            CURSOR.execute("UPDATE dogs SET name = ?, breed = ? WHERE id = ?", (self.name, self.breed, self.id))
+
+    @classmethod
+    def create(cls, name, breed):
+        dog = cls(name, breed)
+        dog.save()
+        return dog
+
+    @classmethod
+    def new_from_db(cls, row):
+        id, name, breed = row
+        dog = cls(name, breed)
+        dog.id = id
+        return dog
+
+    @classmethod
+    def get_all(cls):
+        sql = "SELECT * FROM dogs"
+        CURSOR.execute(sql)
+        rows = CURSOR.fetchall()
+        return [cls.new_from_db(row) for row in rows]
+
+    @classmethod
+    def find_by_name(cls, name):
+        sql = "SELECT * FROM dogs WHERE name = ?"
+        CURSOR.execute(sql, (name,))
+        row = CURSOR.fetchone()
+        if row:
+            return cls.new_from_db(row)
+        return None
+
+    @classmethod
+    def find_by_id(cls, id):
+        sql = "SELECT * FROM dogs WHERE id = ?"
+        CURSOR.execute(sql, (id,))
+        row = CURSOR.fetchone()
+        if row:
+            return cls.new_from_db(row)
+        return None
+
+        
+    @classmethod
+    def find_or_create_by(cls, name, breed):
+        dog = cls.find_by_name(name)
+        if dog:
+            return dog
+        else:
+            return cls.create(name, breed)
+
+    def update(self):
+        self.save()
+
+
+
